14d1f78d35c8c37a7f807e85ec14904010b8fd2c
Sahid Orentino Ferdjaoui <sahid.ferdjaoui@redhat.com>
2017-08-31 12:27:47 +0200

    add pricing tool

---
+++ prices.py
@@ -0,0 +1,90 @@
+import argparse
+
+
+MIN = 0
+MAX = 999999
+PRICES = (
+    (MIN, 500, 1.99),
+    (501, MAX, 0.006),
+)
+
+
+p = argparse.ArgumentParser(description='Trading tools')
+ps = p.add_subparsers(help='Actions')
+p_buy = ps.add_parser('buy', help='Compute price for a buy')
+p_buy.add_argument('buy', metavar='PRICE', type=float, help='Quote price')
+p_buy.add_argument('number', metavar='NUMBER', type=int, help='Number to buy')
+p_sell = ps.add_parser('sell', help='Compute price for a sell')
+p_sell.add_argument('sell', metavar='PRICE', type=float, help='Quote price')
+p_sell.add_argument('number', metavar='NUMBER', type=int, help='Number to sell')
+
+
+
+
+def compute_for_a_buy(trans_amount):
+    return _compute_for('buy', trans_amount)
+
+def compute_for_a_sell(trans_amount):
+    return _compute_for('sell', trans_amount)
+
+def _compute_for(op, trans_amount):
+    trans_fees = 0
+    for rmin, rmax, rfees in PRICES:
+        if (trans_amount > rmin and
+            trans_amount < rmax):
+            if rfees >= 1.0:
+                if op == 'buy':
+                    trans_fees = trans_amount + rfees
+                else:
+                    trans_fees = trans_amount - rfees
+            else:
+                if op == 'buy':
+                    trans_fees = (trans_amount * rfees) + trans_amount
+                else:
+                    trans_fees = (trans_amount * rfees) - trans_amount
+            break
+    return trans_fees
+
+
+def main():
+
+    args = p.parse_args()
+
+    quote_fees = 0
+    trans_fees = 0
+
+    quote_price_to_sell = 0 # Only used when buying
+
+    trans_count = args.number
+    if 'buy' in args:
+        quote_price = args.buy
+        trans_amount = trans_count * quote_price
+        trans_fees = compute_for_a_buy(trans_amount)
+
+        quote_price_to_sell = quote_price
+        while True:
+            quote_price_to_sell += 0.01
+            trans_fees_for_sell = compute_for_a_sell(
+                trans_count * quote_price_to_sell)
+            if (trans_fees_for_sell > trans_fees):
+                break
+    else:
+        quote_price = args.sell
+        trans_amount = trans_count * quote_price
+        trans_fees = compute_for_a_sell(trans_amount)
+    quote_fees = trans_fees / trans_count
+
+    print "Quantity\tPrice\tPrice R.\tTotal\t\tTotal R."
+    print "%d\t\t%.2f\t%.2f\t\t%.2f\t\t%.2f" % (
+        trans_count,
+        quote_price,
+        quote_fees,
+        trans_amount,
+        trans_fees)
+    if quote_price_to_sell:
+        print "Selling: %.2f" % quote_price_to_sell
+        
+
+
+main()
+        
